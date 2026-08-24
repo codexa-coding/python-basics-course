deb5e1369dbfe20a38ca003dcfb62d0fec7b748a
Codexa <alfred.mueller76@atomicmail.io>
2026-08-24 13:42:19 +0200

    Create 02_control_flow_exercises.py

---
+++ exercises/02_control_flow_exercises.py
@@ -0,0 +1,29 @@
+"""
+Exercises: Control flow.
+"""
+
+
+def is_leap_year(year: int) -> bool:
+    """
+    Return True if year is a leap year.
+
+    A leap year is divisible by 4, except years divisible by 100,
+    unless they are also divisible by 400.
+    """
+    pass
+
+
+def fizz_buzz(limit: int) -> list[str]:
+    """
+    Return values from 1 through limit.
+
+    - multiples of 3 become "Fizz"
+    - multiples of 5 become "Buzz"
+    - multiples of both become "FizzBuzz"
+    """
+    pass
+
+
+def count_positive_numbers(numbers: list[int]) -> int:
+    """Return how many numbers are greater than zero."""
+    pass
