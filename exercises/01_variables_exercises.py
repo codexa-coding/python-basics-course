e8171c1d5fe5af4835fabdc62ec92cf9ccddc302
Codexa <alfred.mueller76@atomicmail.io>
2026-08-24 13:42:04 +0200

    Create 01_variables_exercises.py

---
+++ exercises/01_variables_exercises.py
@@ -0,0 +1,20 @@
+"""
+Exercises: Variables and data types.
+
+Implement each function. Run the relevant tests after completion.
+"""
+
+
+def convert_minutes_to_seconds(minutes: int) -> int:
+    """Return the number of seconds in a number of minutes."""
+    pass
+
+
+def full_name(first_name: str, last_name: str) -> str:
+    """Return a full name with each name trimmed and title-cased."""
+    pass
+
+
+def calculate_percentage(part: float, total: float) -> float:
+    """Return part as a percentage of total."""
+    pass
